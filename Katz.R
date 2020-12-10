cf4f504ce3cb08002249b3f14ea1959d45b0bca1
dmitrytoda <dmitry.toda@gmail.com>
2020-12-10 12:08:17 -0600

    1. tested everything on toy corpus, fixed default argument bugs 2. !! faster calculation of unobserved trigram probs due to non-recursive calculation of unobserved bigram probs

--- Katz.R
+++ Katz.R
@@ -37,13 +37,13 @@ find_ngram <- function(ngram, res="count", freqs=dt_ngrams) {
 
 # returns a character vector of all unobserved tail words 
 # for a given ngram
-B <- function(ngram, freqs=dt_ngrams, dic=dict) {
+B <- function(ngram, freqs=dt_ngrams, dict=dict50) {
         n <- length(ngram)+1
         stopifnot(is.character(ngram), n<=max_n, n>=2)
         
         cond <- my_cond(ngram)
         observed_tails <- freqs[[n]][eval(cond)][[n]]
-        dic[!dic %in% observed_tails]
+        dict[!dict %in% observed_tails]
 }
 
 # returns KBO probabilities of unobserved ngrams 
@@ -52,10 +52,6 @@ B_probs <- function(B_words, ngram, freqs=dt_ngrams) {
         probs <- sapply(B_words, function(x) kbo(c(ngram, x)))
         data.table(start=paste(ngram, collapse=' '), end=B_words, prob=probs)
 }
-
-testNG <- c('i')
-b_words <- B(testNG)
-temp <- B_probs(b_words, testNG)
 
 # calculates alpha: probability mass moved from observed
 # (n+1)-grams starting with the given n-gram
@@ -66,8 +62,8 @@ alpha <- function(ngram, freqs=dt_ngrams, disc=0.5) {
         stopifnot(is.character(ngram), n<=max_n, n>=2)
         
         cond <- my_cond(ngram)
-        if(find_ngram(ngram, "bool", freqs=dt_ngrams)) {
-                1 - sum((freqs[[n]][eval(cond), frequency]-disc)) / find_ngram(ngram)
+        if(find_ngram(ngram, "bool", freqs=freqs)) {
+                1 - sum((freqs[[n]][eval(cond), frequency]-disc)) / find_ngram(ngram, freqs=freqs)
         } else {
                 1
         }
@@ -79,7 +75,7 @@ alpha <- function(ngram, freqs=dt_ngrams, disc=0.5) {
 # already preprocessed to tokens like in freqs (with <UNK>)
 # freqs should be a list of observed ngram frequencies
 # disc is absolute discount (same for all levels of n)
-kbo <- function(ngram, freqs=dt_ngrams, disc=0.5) {
+kbo <- function(ngram, freqs=dt_ngrams, disc=0.5, dict=dict50) {
         n <- length(ngram)
         stopifnot(is.character(ngram), n<=max_n, n>=1)
         
@@ -92,14 +88,14 @@ kbo <- function(ngram, freqs=dt_ngrams, disc=0.5) {
         ## 2A: if OBSERVED, return c*(this ngram) / c(the beginning of ngram)
         if(find_ngram(ngram, "bool", freqs)) {
                 print(paste('Observed:', paste(ngram, collapse=' ')))
-                return((find_ngram(ngram)-disc) / find_ngram(ngram[1:n-1]))
+                return((find_ngram(ngram, "count", freqs)-disc) / find_ngram(ngram[1:n-1], "count", freqs))
         }
         
         ## 2B: if UNOBSERVED
         print(paste("Unobserved:", paste(ngram, collapse = ' ')))
         # find observed ngrams that start with the same n-1 words
         my_alpha <- alpha(ngram[1:n-1], freqs, disc)
-        my_B <- B(ngram[1:n-1], freqs)
+        my_B <- B(ngram[1:n-1], freqs, dict=dict)
         
         # 2Bi: unobserved BIgram: return alpha, distributed over 
         # all B-words according to their MLE (within B)
@@ -120,22 +116,34 @@ kbo <- function(ngram, freqs=dt_ngrams, disc=0.5) {
                 B2 <- B(ngram[2], freqs, my_B)
                 A2 <- my_B[! my_B %in% B2]
                 
-                A2_probs <- 
+                B2 <- lapply(B2, function(x) c(ngram[2:(n-1)], x))
+                A2 <- lapply(A2, function(x) c(ngram[2:(n-1)], x))
+                #return(list(A2, B2))
                 
+                # prob = alpha * numerator / denominator
+                numerator <- kbo(ngram[2:n], freqs = freqs, disc = disc, dict = dict)
+                Bgrams <- lapply(my_B, function(x) c(ngram[2:(n-1)], x))
+                denominator <- sum(bi_probs2(A2, B2, freqs=freqs, disc=disc, dict=dict))
+                my_alpha * numerator / denominator
         }
         
         
 }
 
-temp <- kbo(c('i', 'love', 'hui'))
+bi_probs <- function (bigrams, freqs=freqs, disc, dict) {
+        sapply(bigrams, kbo, freqs=freqs, disc=disc, dict=dict)
+}
 
-kbo(c('i', 'love', 'you'))
-
-kbo(c('love', 'hui'))
-
-temp <- kbo(c("i", "love", "hui"))
-temp
-
-temp <- kbo(c("you", "my", 'dear', 'lily', '<UNK>'))
-temp
+bi_probs2 <- function (A, B, freqs, disc, dict) {
+        # probabilities of observed bigrams A
+        a_probs <- sapply(A, kbo, freqs, disc, dict)
+        
+        # probabilities of unobserved bigrams B
+        bi_alpha <- alpha(B[[1]][1], freqs, disc)
+        tails <- sapply(B, function(x) x[length(x)])
+        tails <- freqs[[1]][.(tails)]
+        denom <- sum(tails[,frequency])
+        b_probs <- bi_alpha * tails$frequency / denom
+        return(c(a_probs, b_probs))
+}
           
